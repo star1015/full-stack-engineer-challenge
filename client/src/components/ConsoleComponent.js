8aa4c6800e08f02f4b88d664d830bf21742d1396
star1015 <star1015@gmail.com>
2021-03-17 16:31:19 +0800

    Code format.

--- client/src/components/ConsoleComponent.js
+++ client/src/components/ConsoleComponent.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const ConsoleComponent = (props) => {
   return <Container>Dashboard {props.capsules}</Container>;
--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 import ConsoleComponent from '../components/ConsoleComponent';
 import ControlComponent from '../components/ControlComponent';
-import { getAllCapsules, getLandingPadByID } from "../redux/space/action";
+import { getAllCapsules, getLandingPadByID } from '../redux/space/action';
 
 const Dashboard = (props) => {
 
